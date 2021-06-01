2e2c7f3cc3e5d5d923ee54b47ce9c0be157e6ac0
AlbertoVf <8244709-AlbertoVf1@users.noreply.gitlab.com>
2021-06-01 16:25:15 +0200

    Reset game and score

--- main.py
+++ main.py
@@ -44,7 +44,14 @@ def randomSnack(rows, item):
 
 
 def message_box(subject, content):
-    pass
+    root = tk.Tk()
+    root.attributes("-topmost", True)
+    root.withdraw()
+    messagebox.showinfo(subject, content)
+    try:
+        root.destroy()
+    except:
+        pass
 
 
 def main():
@@ -65,6 +72,14 @@ def main():
         if s.body[0].pos == snack.pos:
             s.addCube()
             snack = Cube(randomSnack(rows, s), color=(0, 255, 0))
+
+        for x in range(len(s.body)):
+            if s.body[x].pos in list(map(lambda z: z.pos, s.body[x + 1:])):
+                message_box('You Lost!',
+                            f'Play again...\n Score:{len(s.body)}')
+                s.reset((10, 10))
+                break
+
         redrawWindow(win)
 
 
--- snake.py
+++ snake.py
@@ -58,7 +58,12 @@ class Snake(object):
                     c.move(c.dirx, c.diry)
 
     def reset(self, pos):
-        pass
+        self.head = Cube(pos)
+        self.body = []
+        self.body.append(self.head)
+        self.turns = {}
+        self.dirnx = 0
+        self.dirny = 1
 
     def addCube(self):
         tail = self.body[-1]
